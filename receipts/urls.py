b0affda3f35de11f27ca84343017afdc163dc478
jaspreet <jaspreetsangha204@yahoo.com>
2023-06-01 15:09:25 -0700

    Feature 12, 13 and 14 Completed

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from receipts.views import receipts_list, create_receipt
+from receipts.views import (
+    receipts_list,
+    create_receipt,
+    expense_category_list,
+    accounts_list,
+    create_category,
+    create_account,
+)
 
 urlpatterns = [
     path("", receipts_list, name="home"),
     path("create/", create_receipt, name="create_receipt"),
+    path("categories/", expense_category_list, name="category_list"),
+    path("accounts/", accounts_list, name="account_list"),
+    path("categories/create/", create_category, name="create_category"),
+    path("accounts/create/", create_account, name="create_account"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from receipts.models import Receipt
+from django.shortcuts import render, redirect
+from receipts.models import Receipt, ExpenseCategory, Account
 from django.contrib.auth.decorators import login_required
-from receipts.forms import ReceiptForm
+from receipts.forms import ReceiptForm, ExpenseCategoryForm, AccountForm
 
 # Create your views here.
 
@@ -28,3 +28,53 @@ def create_receipt(request):
         form = ReceiptForm()
     context = {"form": form}
     return render(request, "receipts/create.html", context)
+
+
+@login_required
+def expense_category_list(request):
+    categories = ExpenseCategory.objects.filter(owner=request.user)
+    context = {
+        "categories": categories,
+    }
+    return render(request, "categories/list.html", context)
+
+
+@login_required
+def accounts_list(request):
+    accounts = Account.objects.filter(owner=request.user)
+    context = {
+        "accounts": accounts,
+    }
+    return render(request, "accounts/list.html", context)
+
+
+@login_required
+def create_category(request):
+    if request.method == "POST":
+        form = ExpenseCategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(False)
+            category.owner = request.user
+            category.save()
+        return redirect("category_list")
+    else:
+        form = ExpenseCategoryForm()
+    context = {"form": form}
+    return render(request, "categories/create.html", context)
+
+
+@login_required
+def create_account(request):
+    if request.method == "POST":
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(False)
+            account.owner = request.user
+            account.save()
+        return redirect("account_list")
+    else:
+        form = AccountForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/create.html", context)
