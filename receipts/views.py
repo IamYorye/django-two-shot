9628dbb537288d21d624e9c9226585b390c44525
jaspreet <jaspreetsangha204@yahoo.com>
2023-06-01 12:04:36 -0700

    Feature 5 Complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from receipts.models import Receipt
 
 # Create your views here.
+
+
+def receipts_list(request):
+    receipts = Receipt.objects.all()
+    context = {
+        "receipts": receipts,
+    }
+    return render(request, "receipts/list.html", context)
